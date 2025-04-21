048ad3167913c62fd46ea91cfd0920f132e4a1f0
padmalama <prlama55@gmail.com>
2025-04-22 07:32:57 +0900

    Correction

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -1,4 +1,4 @@
 import avatar from "./avatar";
 import button from "./button";
-const componenst = { ...avatar, ...button };
-export default componenst;
+const components = { ...avatar, ...button };
+export default components;
